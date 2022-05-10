d1272472acc1635df9235e07f09506e24832f8a2
Aaron <addaeaaron28@gmail.com>
2022-05-10 11:54:17 +0000

    update: added the use case hook to the search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import SearchMain from "./components/SearchMain";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <SearchMain />
     </div>
   );
 }
--- src/components/searchMain.js
+++ src/components/searchMain.js
@@ -1,5 +1,24 @@
-const searchMain = () => {
-  return <div></div>;
+import { useState } from "react";
+import "../components/style.css";
+
+const SearchMain = () => {
+  const [searchItem, setSearchItem] = useState("");
+
+  return (
+    <div className="wrap">
+      <div className="search">
+        <input
+          type="search"
+          placeholder="type city name.."
+          id="search"
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+        />
+      </div>
+
+      <button className="searchButton">search </button>
+    </div>
+  );
 };
 
-export default searchMain;
+export default SearchMain;
